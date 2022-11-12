f7cf5c777233b4e55585a203052703c3ea436221
Victor Hugo de Almeida Baraldi <victor_baraldi@hotmail.com>
2022-11-12 12:16:58 -0300

    Nav and media

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,31 @@
 const btnMobile = document.querySelector(".btn-mobile");
+const sobreBtn= document.querySelector(".sobre-btn");
+const habBtn= document.querySelector(".hab-btn");
+const formBtn= document.querySelector(".form-btn");
+const projBtn= document.querySelector(".proj-btn");
 
 btnMobile.addEventListener("click", function () {
   const nav = document.querySelector(".header-nav");
   nav.classList.toggle("active");
 });
+
+sobreBtn.addEventListener("click", function () {
+  const nav = document.querySelector(".header-nav");
+  nav.classList.toggle("active");
+});
+
+formBtn.addEventListener("click", function () {
+  const nav = document.querySelector(".header-nav");
+  nav.classList.toggle("active");
+});
+
+habBtn.addEventListener("click", function () {
+  const nav = document.querySelector(".header-nav");
+  nav.classList.toggle("active");
+});
+
+projBtn.addEventListener("click", function () {
+  const nav = document.querySelector(".header-nav");
+  nav.classList.toggle("active");
+});
+
